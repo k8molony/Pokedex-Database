bfca7a136430aeaa71a550c6e41a31505b957f36
Katy Molony <k8molony@att.net>
2021-07-14 17:55:04 -0400

    Added 4 pokemon to PokemonList
    Included name, number, height, type, weakness and evolution for Bulbasaur, Charmander, Squirtle, and Pikachu.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,34 @@
-
+let pokemonList [
+  {
+    name: 'Bulbasaur',
+    number: 001,
+    height: 2.4,
+    type: ['grass', 'poison'],
+    weakness: ['fire', 'psychic', 'flying', 'ice'],
+    evolutions: ['Ivysaur', 'Venusaur'],
+  },
+  {
+    name: 'Charmander',
+    number: 004,
+    height: 2,
+    tyoe: 'fire',
+    weakness: ['water', 'ground', 'rock'],
+    evolutions: ['Charmeleon', 'Charizard'],
+  },
+  {
+    name: 'Squirtle',
+    number: 007,
+    height: 1.08,
+    type: 'water',
+    weakness: ['grass', 'electric'],
+    evolutions: ['Wartortle', 'Blastoise'],
+  },
+  {
+    name: 'Pikachu',
+    number: 025,
+    height: 1.04,
+    type: 'electric',
+    weakness: 'ground',
+    evolutions: ['Pichu', 'Raichu'],
+  }
+];
