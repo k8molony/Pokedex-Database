71caeea75781c5a16fa1d354c96153e8f1c692f1
Katy Molony <k8molony@att.net>
2021-07-22 12:42:55 -0400

    Wrapped pokemonList in IIFE and added pokemonRepository

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,52 @@
-let pokemonList = [
-  {
-    name: 'Bulbasaur',
-    number: 001,
-    height: 2.4,
-    type: ['grass', 'poison'],
-    weakness: ['fire', 'psychic', 'flying', 'ice'],
-    evolutions: ['Ivysaur', 'Venusaur'],
-  },
-  {
-    name: 'Charmander',
-    number: 004,
-    height: 2,
-    type: 'fire',
-    weakness: ['water', 'ground', 'rock'],
-    evolutions: ['Charmeleon', 'Charizard'],
-  },
-  {
-    name: 'Squirtle',
-    number: 007,
-    height: 1.08,
-    type: 'water',
-    weakness: ['grass', 'electric'],
-    evolutions: ['Wartortle', 'Blastoise'],
-  },
-  {
-    name: 'Pikachu',
-    number: 025,
-    height: 1.04,
-    type: 'electric',
-    weakness: 'ground',
-    evolutions: ['Pichu', 'Raichu'],
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: 'Bulbasaur',
+      number: 001,
+      height: 2.4,
+      type: ['grass', 'poison'],
+      weakness: ['fire', 'psychic', 'flying', 'ice'],
+      evolutions: ['Ivysaur', 'Venusaur'],
+    },
+    {
+      name: 'Charmander',
+      number: 004,
+      height: 2,
+      type: 'fire',
+      weakness: ['water', 'ground', 'rock'],
+      evolutions: ['Charmeleon', 'Charizard'],
+    },
+    {
+      name: 'Squirtle',
+      number: 007,
+      height: 1.08,
+      type: 'water',
+      weakness: ['grass', 'electric'],
+      evolutions: ['Wartortle', 'Blastoise'],
+    },
+    {
+      name: 'Pikachu',
+      number: 025,
+      height: 1.04,
+      type: 'electric',
+      weakness: 'ground',
+      evolutions: ['Pichu', 'Raichu'],
+    }
+  ];
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
   }
-];
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
 
 //Creates list of pokemon and their sizes.
 for (let i=0; i < pokemonList.length; i++) {
@@ -44,6 +59,7 @@ for (let i=0; i < pokemonList.length; i++) {
     }
 }
 
+//Creates list of Pokemon and their types
 pokemonList.forEach(function(pokemon) {
   document.write('<p>' + pokemon.name + ' is ' + pokemon.type + ' type.' + '</p>');
 })
